56353a4ec5e0ffec4720c4490a1cf5b1189792d8
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-17 11:35:55 +0600

    ***header file setup done ***

--- React-Knowledge-Cafe/src/App.jsx
+++ React-Knowledge-Cafe/src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-
+import Header from './component/Header'
 function App() {
 
 
   return (
     <>
       
-      <h1 className='text-6xl text-center bg-red-300'>Knowledge Cafe React project</h1>
+<Header></Header>
     
     </>
   )
---
+++ React-Knowledge-Cafe/src/component/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+                  <h1 className='text-6xl text-center bg-red-300'>Knowledge Cafe React project</h1>
+        </div>
+    );
+};
+
+export default Header;
