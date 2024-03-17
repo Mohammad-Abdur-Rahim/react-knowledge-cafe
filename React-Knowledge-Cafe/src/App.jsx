9a29be122080bf445795187febeb51af06bf81ab
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-17 11:30:43 +0600

    ***Setup Done To Work Project React ***

---
+++ React-Knowledge-Cafe/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-center bg-red-300'>Knowledge Cafe React project</h1>
+    
+    </>
+  )
+}
+
+export default App
