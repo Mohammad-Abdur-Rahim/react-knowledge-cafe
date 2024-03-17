d3f80f0f53f8c33a6c59e0bbe3db64011710b176
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-17 14:05:19 +0600

    **Bookmark state declare   ***

--- React-Knowledge-Cafe/src/App.jsx
+++ React-Knowledge-Cafe/src/App.jsx
@@ -3,20 +3,23 @@ import './App.css'
 import Header from './component/Header/Header'
 import Blogs from './component/Blog/Blogs'
 import Bookmark from './component/Bookmark/Bookmark'
+import { useState } from 'react'
 function App() {
-
+const [bookMark,setBookMark]=useState([]);
 
   return (
-    <>
+ 
       
+<div className='max-w-screen-2xl mx-auto'>
 <Header></Header>
 <div className='md:flex'>
 <Blogs></Blogs>
 
 <Bookmark></Bookmark>
 </div>
+</div>
     
-    </>
+  
   )
 }
 
--- React-Knowledge-Cafe/src/component/BlogShow/Blogdata.jsx
+++ React-Knowledge-Cafe/src/component/BlogShow/Blogdata.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'; // ES6
+import { BsBookmarkCheckFill } from "react-icons/bs";
 const Blogdata = ({ blogData }) => {
     const { title, cover, author_img, posted_date, reading_time, hashtag } = blogData;
 
     return (
-        <div>
-            <img className='w-3/4' src={cover} alt={`Cover Picture The Title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover Picture The Title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-52' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -15,9 +16,10 @@ const Blogdata = ({ blogData }) => {
                 </div>
                 <div>
                     <span className='text-2xl'>{reading_time}Min Read</span>
+                    <button className='ml-2 text-2xl'><BsBookmarkCheckFill /></button>
                 </div>
             </div>
-            <h2 className="text-5xl">{title}</h2>
+            <h2 className="text-5xl mb-4">{title}</h2>
             <p className='text-2xl'>
                 {
                     hashtag.map((count,idx)=><span key={idx}><a href='#'>#{count}</a></span>)
