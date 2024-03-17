22abf033d36dff5978c5959ab7bb7cd6955eb195
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-17 13:38:35 +0600

    **Blog data hastag set done  ***

--- React-Knowledge-Cafe/src/component/BlogShow/Blogdata.jsx
+++ React-Knowledge-Cafe/src/component/BlogShow/Blogdata.jsx
@@ -1,15 +1,33 @@
 import PropTypes from 'prop-types'; // ES6
-const Blogdata = ({blogData}) => {
-  const {title} = blogData;
+const Blogdata = ({ blogData }) => {
+    const { title, cover, author_img, posted_date, reading_time, hashtag } = blogData;
 
     return (
         <div>
-            <h2 className="text5xl">{title}</h2>
+            <img src={cover} alt={`Cover Picture The Title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-52' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-3xl'>Author</h3>
+                        <p className='text-3xl'>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span className='text-2xl'>{reading_time}Min Read</span>
+                </div>
+            </div>
+            <h2 className="text-5xl">{title}</h2>
+            <p className='text-2xl'>
+                {
+                    hashtag.map(count=><span><a href='#'>#{count}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
-Blogdata.propTypes={
+Blogdata.propTypes = {
     blogData: PropTypes.object.isRequired
 }
 export default Blogdata;
